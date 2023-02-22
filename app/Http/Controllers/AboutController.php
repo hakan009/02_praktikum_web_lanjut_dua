7a72c6ef9701515a849864955b958a5219ce8463
Hakan Alif Pramudya <hakanalifpramudya13@gmail.com>
2023-02-22 10:14:36 +0700

    SingleController

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function about() {
+        return 'Nama: Hakan Alif P <br> NIM: 2141720218';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +23,16 @@ Route::get('/', function () {
 });
 */
 
+
 //controller index
-Route::get('/', [PageController::class, 'index']);
+//Route::get('/', [PageController::class, 'index']);
 
 //controller about
-Route::get('/about', [PageController::class, 'about']);
+//Route::get('/about', [PageController::class, 'about']);
 
 //controller id
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+//Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
